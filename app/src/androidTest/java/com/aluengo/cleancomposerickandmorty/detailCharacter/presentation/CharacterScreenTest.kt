5b4ad277db2de595804a858a782a5c187a5a7f51
aluengo <alberto@attomic.es>
2023-06-25 17:40:30 +0200

    refactor: detail UI Test

--- app/src/androidTest/java/com/aluengo/cleancomposerickandmorty/detailCharacter/presentation/CharacterScreenTest.kt
+++ app/src/androidTest/java/com/aluengo/cleancomposerickandmorty/detailCharacter/presentation/CharacterScreenTest.kt
@@ -2,29 +2,20 @@ package com.aluengo.cleancomposerickandmorty.detailCharacter.presentation
 
 import androidx.activity.compose.setContent
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SnackbarHostState
-import androidx.compose.material3.Surface
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.test.assertCountEquals
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onAllNodesWithText
 import androidx.compose.ui.test.onNodeWithText
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.aluengo.cleancomposerickandmorty.core.data.MockData
 import com.aluengo.cleancomposerickandmorty.core.di.LocalStorageModule
 import com.aluengo.cleancomposerickandmorty.core.di.NetworkModule
 import com.aluengo.cleancomposerickandmorty.core.navigation.CharacterItemInput
-import com.aluengo.cleancomposerickandmorty.core.navigation.NavRoutes
-import com.aluengo.cleancomposerickandmorty.core.ui.theme.CleanComposeRickAndMortyTheme
 import com.aluengo.cleancomposerickandmorty.listCharacters.data.ListCharactersResponse
 import com.aluengo.cleancomposerickandmorty.main.MainActivity
 import dagger.hilt.android.testing.HiltAndroidRule
@@ -59,26 +50,14 @@ class CharacterScreenTest {
         expectedCharacter = mockData.createListCharactersResponse().results.first()
 
         composeRule.activity.setContent {
-            val navController = rememberNavController()
-            CleanComposeRickAndMortyTheme {
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    val snackbarHostState = remember { SnackbarHostState() }
-                    NavHost(
-                        navController, startDestination = NavRoutes.Character.route
-                    ) {
-                        composable(route = NavRoutes.Character.route) {
-                            val viewModel = hiltViewModel<CharacterViewModel>()
-
-                            CharacterScreen(
-                                viewModel.viewState.collectAsState().value,
-                                CharacterItemInput(expectedCharacter.id, expectedCharacter.name),
-                                snackbarHostState,
-                                viewModel::submitIntent
-                            )
-                        }
-                    }
-                }
-            }
+            val snackbarHostState = remember { SnackbarHostState() }
+            val viewModel = hiltViewModel<CharacterViewModel>()
+            CharacterScreen(
+                viewModel.viewState.collectAsState().value,
+                CharacterItemInput(expectedCharacter.id, expectedCharacter.name),
+                snackbarHostState,
+                viewModel::submitIntent
+            )
         }
     }
 
